f4e2d308e8e177fa9b8f4aab38c7bcee10b1088c
Samantha Cabrera <samantha.n.cabrera@gmail.com>
2024-09-12 16:16:29 -0600

    added styling to footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,18 +3,14 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <footer className="fixed bottom-0">
-      <div className="space-x-12">
-        <Link 
-          to="/" 
-        >
-          Home
-        </Link>
-        <Link 
-          to="/profile" 
-        >
-          Profile
-        </Link>
+    <footer className="bg-black text-white py-4 mt-auto">
+      <div className="container mx-auto flex justify-between items-center">
+        <div>
+          <Link to="/" className="hover:text-gray-400 transition-colors">Home</Link>
+        </div>
+        <div>
+          <Link to="/profile" className="hover:text-gray-400 transition-colors">Profile</Link>
+        </div>
       </div>
     </footer>
   );
